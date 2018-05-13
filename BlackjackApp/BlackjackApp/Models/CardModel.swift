4eb959842b5ef4af2ada7fd8094b5104de2e6dfa
System Administrator <root@Mac-mini.local>
2018-05-13 07:27:40 -0700

    First commit, half done

---
+++ BlackjackApp/BlackjackApp/Models/CardModel.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  BlackjackApp
+//
+//  Created by System Administrator on 5/13/18.
+//  Copyright © 2018 Universidad de Lima. All rights reserved.
+//
+
+class Card {
+    var IsRevealed: bool;
+}
---
+++ BlackjackApp/BlackjackApp/Views/CardView.swift
@@ -0,0 +1,13 @@
+//
+//  CardView.swift
+//  BlackjackApp
+//
+//  Created by System Administrator on 5/13/18.
+//  Copyright © 2018 Universidad de Lima. All rights reserved.
+//
+
+import Cocoa
+
+class CardView: UIView {
+
+}
